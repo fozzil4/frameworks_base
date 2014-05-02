d7503e4d89cf328622f31a5b0cf806523b7b1526
Cristiano Matos <cristianobmatos@gmail.com>
2014-05-02 12:19:26 -0300

    PEEK: configurable pickup timeout (1/2)

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3525,6 +3525,12 @@ public final class Settings {
         public static final String LOCKSCREEN_ROTATION_ENABLED = "lockscreen_rotation_enabled";
 
         /**
+         * Set the timeout of peek when pikcing up the device
+         * @hide
+         */
+        public static final String PEEK_PICKUP_TIMEOUT = "peek_pickup_timeout";
+
+        /**
           * Always show the battery status on the lockscreen
           * @hide
           */
